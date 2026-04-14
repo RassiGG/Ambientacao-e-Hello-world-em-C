6572351c6b7978dc6bdb5491fa0a333c40399f0c
Rassi <leonardoali2008@gmail.com>
2026-04-14 18:40:32 -0400

    teste

--- leituradeidadealturaenome.c
+++ leituradeidadealturaenome.c
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h> //Avisa que o computador que vamos usar comandos de entrada e saída
 int main(){
 int idadeUsuario;
